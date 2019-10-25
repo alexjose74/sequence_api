06b136eb7c25813e685ecbd4a6e9a72b35071749
Alex Jose <alex.jose@cerner.com>
2019-10-25 12:16:58 +0530

    Documentation

--- app/controllers/sequences_controller.rb
+++ app/controllers/sequences_controller.rb
@@ -1,6 +1,10 @@
 class SequencesController < ApplicationController
+  # This should ideally be removed when an oAuth authentication system is setup which authenticates the system accessing the API
   skip_before_action :verify_authenticity_token
 
+  # Public: Handles the POST call to save the sequence data
+  #
+  # Returns a status message indicating the success of the save action
   def create
     sequence_details = params[:sequences]
     sequence_details.each do |sequence_element|
